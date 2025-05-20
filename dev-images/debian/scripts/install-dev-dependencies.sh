45f76f83b6c0097d63781d19da51edc4c9d7027c
Thomas Bell <thomas@thomasbell.io>
2025-05-20 13:25:36 -0600

    updated the dev dependencies

--- dev-images/debian/scripts/install-dev-dependencies.sh
+++ dev-images/debian/scripts/install-dev-dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-apt update -y && \
-apt install -y dnsutils && \
-apt install -y pass && \
-apt install -y ack gcc make man-db git ssh openssh-client curl zsh sudo gosu tmux && \
-apt install -y xauth x11-apps xclip && \
-chsh -s $(which zsh) && \
-mkdir -p /root/.ssh && \
-mkdir -p /root/code/ && \
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && \
-rm -rf /opt/nvim && \
-tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+apt update -y &&
+    apt install -y dnsutils &&
+    apt install -y pass &&
+    apt install -y ack ripgrep gcc make man-db git ssh openssh-client curl zsh sudo gosu tmux &&
+    apt install -y xauth x11-apps xclip &&
+    chsh -s $(which zsh) &&
+    mkdir -p /root/.ssh &&
+    mkdir -p /root/code/ &&
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz &&
+    rm -rf /opt/nvim &&
+    tar -C /opt -xzf nvim-linux-x86_64.tar.gz
